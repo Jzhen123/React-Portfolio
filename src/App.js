019cd102b19a3e9ebf3935d01e7cf3360ceabe8e
Jzhen123 <jiayuzheng01@gmail.com>
2021-04-25 11:48:40 -0400

    Moved about section to a new component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import Splash from './Splash';
 import Yosemite from './Yosemite.png';
 import './App.css';
+import About from './About';
 
 function App() {
     return (
-        <Splash image={Yosemite} />
+        <>
+            <Splash image={Yosemite} />
+            <About image={Yosemite} />
+        </>
     )
 }
 
--- src/Splash.js
+++ src/Splash.js
@@ -15,17 +15,12 @@ function Splash({ image }) {
                 <div className="text">
                     <h1>Hi,<br></br>I'm Jimmy Zheng!</h1>
                     <h3 className="mt-2">
-                        <i>Full Stack Web Developer continuously improving <br></br>
-                            to reach new heights</i>
+                        <i>Full Stack Web Developer continuously improving <br></br> to reach new heights</i>
                     </h3>
                     <a href="#about" className="btn btn-outline-light rounded-0">About Me</a>
                 </div>
-                <Scroll />
-            </header>
 
-            {/* Below the Fold */}
-            <header id="about">
-                <img src={mainImage}></img>
+                <Scroll />
             </header>
         </>
     )
